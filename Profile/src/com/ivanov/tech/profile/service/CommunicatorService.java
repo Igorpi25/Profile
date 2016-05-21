95110f0cba41e5f413320959bc61fee7a380f443
Igorpi25 <Igorpi25@gmail.com>
2016-05-22 02:29:41 +1000

    Communicator changed

--- Profile/src/com/ivanov/tech/profile/service/CommunicatorService.java
+++ Profile/src/com/ivanov/tech/profile/service/CommunicatorService.java
@@ -5,7 +5,13 @@ import java.util.ArrayList;
 import com.ivanov.tech.communicator.service.TransportBase;
 
 public class CommunicatorService extends com.ivanov.tech.communicator.service.CommunicatorService{
-
+	
+	//---------------Communicator Demo URL-------------
+    public final static String URL_PROTOCOL="ws://";
+    public final static String URL_DOMEN="igorpi25.ru";  
+    public final static String URL_PORT=":8001";//Websocket server port
+    public final static String URL_SERVER=URL_PROTOCOL+URL_DOMEN+URL_PORT;
+    
 	@Override
 	public ArrayList<TransportBase> createTransports() {
 		
@@ -16,5 +22,12 @@ public class CommunicatorService extends com.ivanov.tech.communicator.service.Co
 		
 		return transports;
 	}
+
+	@Override
+	public String getServerUrl() {
+		return URL_SERVER;
+	}
+	
+	
 
 }
